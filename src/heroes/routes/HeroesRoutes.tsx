87a8bfcd378d57a55e392aebde059e2d8bc32f7a
snofamv <fniclous97@gmail.com>
2025-01-16 21:20:08 -0300

    fix docs

--- src/heroes/routes/HeroesRoutes.tsx
+++ src/heroes/routes/HeroesRoutes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { DcPage, MarvelPage, HeroPage, SearchPage } from "../";
 import { Navbar } from "../../ui";
 
@@ -8,11 +8,12 @@ export const HeroesRoutes = (): JSX.Element => {
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path="marvel" element={<MarvelPage />} />
-          <Route path="dc" element={<DcPage />} />
-          <Route path="search" element={<SearchPage />} />
-          <Route path="hero/:heroId" element={<HeroPage />} />
-          <Route path="/" element={<Navigate to={"/marvel"} />} />
+          <Route path="/" element={<MarvelPage />} />
+          <Route path="/marvel" element={<MarvelPage />} />
+          <Route path="/dc" element={<DcPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/hero/:heroId" element={<HeroPage />} />
+          {/* <Route path="/" element={<Navigate to={"/marvel"} />} /> */}
 
           {/* search hero by id */}
         </Routes>
