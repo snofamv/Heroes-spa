7e874011dbb638b0a91e80645dd04d810f5bb19a
snofamv <fniclous97@gmail.com>
2025-01-17 00:36:42 -0300

    fix

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -19,7 +19,6 @@ export const Navbar = () => {
         <button
           onClick={() => setShowToggle(!showToggle)}
           className="navbar-toggler"
-       
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -45,35 +44,14 @@ export const Navbar = () => {
           </div>
         </div>
       </div>
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link text-primary">Fabian</span>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
+        </ul>
+      </div>
     </nav>
-    // <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-    //   <Link className="navbar-brand" to="/">
-    //     Asociaciones
-    //   </Link>
-
-    //   <div className="navbar-collapse">
-    //     <div className="navbar-nav">
-    //       <NavLink className="nav-item nav-link" to={`/marvel`}>
-    //         Marvel
-    //       </NavLink>
-
-    //       <NavLink className="nav-item nav-link" to={`/dc`}>
-    //         DC
-    //       </NavLink>
-    //       <NavLink className="nav-item nav-link" to={`/search`}>
-    //         Search
-    //       </NavLink>
-    //     </div>
-    //   </div>
-
-    //   <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-    //     <ul className="navbar-nav ml-auto">
-    //       <span className="nav-item nav-link text-primary">Fabian</span>
-    //       <button className="nav-item nav-link btn" onClick={handleLogout}>
-    //         Logout
-    //       </button>
-    //     </ul>
-    //   </div>
-    // </nav>
   );
 };
